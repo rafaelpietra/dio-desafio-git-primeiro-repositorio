138be76e6f8144303bb940ab0d5e0f1f47dc0368
rafaelpietra <rafaelpietra@gmail.com>
2022-04-18 16:00:35 -0300

    Terminando exercicio de set e array

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,25 +1,31 @@
-function getAdmins(map) {
-    let admins = [];
-    for([key, value] of map) {
-        if (value === 'Admin') {
-            admins.push(key)
-        }
-    }
-    return admins;
+
+// function getAdmins(map) {
+//     let admins = [];
+//     for([key, value] of map) {
+//         if (value === 'Admin') {
+//             admins.push(key)
+//         }
+//     }
+//     return admins;
+// }
+
+// const usuarios = new Map();
+
+// usuarios.set('Luiz','Admin');
+// usuarios.set('Stephany','Admin');
+// usuarios.set('Jorge','User');
+// usuarios.set('Natália','Admin');
+
+
+// console.log(getAdmins(usuarios)); 
+
+
+const meuArray = [30,30,40,5,223,2049,3034,5];
+
+function valoresUnicos(array) {
+    const mySet = new Set(array);
+
+    return [...mySet];
 }
-
-const usuarios = new Map();
-
-usuarios.set('Luiz','Admin');
-usuarios.set('Stephany','Admin');
-usuarios.set('Jorge','User');
-usuarios.set('Natália','Admin');
-
-
-console.log(getAdmins(usuarios)); 
-
-
-//const meuArray = [30,30,40,5,223,2049,3034,5];
-
-//function valoresUnicos(array) {
     
+console.log(valoresUnicos(meuArray));
